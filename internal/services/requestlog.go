9e2aaa27874c0a65ac99a205e166ee045863019e
Denis Mishankov <mishankov@mail.com>
2025-06-09 17:49:25 +0300

    make actual save through channel

--- internal/services/requestlog.go
+++ internal/services/requestlog.go
@@ -1,16 +1,29 @@
 package services
 
 import (
+	"log"
+
 	"github.com/jmoiron/sqlx"
 	"github.com/mishankov/proxymini/internal/requestlog"
 )
 
 type RequestLogService struct {
-	db *sqlx.DB
+	db           *sqlx.DB
+	requestLogCh chan requestlog.RequestLog
 }
 
 func NewRequestLogService(db *sqlx.DB) *RequestLogService {
-	return &RequestLogService{db: db}
+	ch := make(chan requestlog.RequestLog)
+	rls := &RequestLogService{db: db, requestLogCh: ch}
+	go func() {
+		for l := range ch {
+			err := rls.save(l)
+			if err != nil {
+				log.Println("Error saving request log from channel:", err)
+			}
+		}
+	}()
+	return rls
 }
 
 func (rls *RequestLogService) GetList() ([]requestlog.RequestLog, error) {
@@ -35,13 +48,18 @@ func (rls *RequestLogService) GetList() ([]requestlog.RequestLog, error) {
 	return res, nil
 }
 
-func (rls *RequestLogService) Save(rl requestlog.RequestLog) error {
+func (rls *RequestLogService) save(rl requestlog.RequestLog) error {
 	_, err := rls.db.Exec(
 		"INSERT INTO request_log (id, time, method, url, request_headers, request_body, status, response_headers, response_body) VALUES (?,?,?,?,?,?,?,?,?)",
 		rl.ID, rl.Time, rl.Method, rl.URL, rl.RequestHeaders, rl.RequestBody, rl.Status, rl.ResponseHeaders, rl.ResponseBody,
 	)
 
 	return err
+}
+
+func (rls *RequestLogService) Save(rl requestlog.RequestLog) error {
+	rls.requestLogCh <- rl
+	return nil
 }
 
 func (rls *RequestLogService) DeleteAll() error {
